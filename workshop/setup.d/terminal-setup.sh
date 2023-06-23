20ddc128a36e84b478d0ba6aa722ee5c45f43799
Timo Salm <tsalm@vmware.com>
2023-06-23 14:03:43 +0200

    Initial Workshop setup

---
+++ workshop/setup.d/terminal-setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -x
+set +e
+
+mkdir ~/exports
+
+kubectl() {
+    if [[ $@ == *"secret"* ]]; then
+        command echo "No resources found in $SESSION_NAMESPACE namespace."
+    else
+        command kubectl "$@"
+    fi
+}
+
+k() {
+    if [[ $@ == *"secret"* ]]; then
+        command echo "No resources found in $SESSION_NAMESPACE namespace."
+    else
+        command kubectl "$@"
+    fi
+}
