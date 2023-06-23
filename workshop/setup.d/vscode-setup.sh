dd5d30c18ab22281e7f343157addfe832c324d6b
Timo Salm <tsalm@vmware.com>
2023-06-23 14:29:01 +0200

    Initial Workshop setup

--- workshop/setup.d/vscode-setup.sh
+++ workshop/setup.d/vscode-setup.sh
@@ -2,4 +2,4 @@
 set -x
 set +e
 
-jq ". + { \"redhat.telemetry.enabled\": false, \"vs-kubernetes.ignore-recommendations\": true}" /home/eduk8s/.local/share/code-server/User/settings.json | sponge /home/eduk8s/.local/share/code-server/User/settings.json
+jq ". + { \"redhat.telemetry.enabled\": false, \"vs-kubernetes.ignore-recommendations\": true, \"tanzu-app-accelerator.tanzuApplicationPlatformGuiUrl\":  \"https://tap-gui.${TAP_INGRESS}\"}" /home/eduk8s/.local/share/code-server/User/settings.json | sponge /home/eduk8s/.local/share/code-server/User/settings.json
