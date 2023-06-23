17797b80b19c1b8550444d935b9342b50915d67f
Timo Salm <tsalm@vmware.com>
2023-06-23 14:03:43 +0200

    Initial Workshop setup

---
+++ workshop/setup.d/resources-setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -x
+set +e
+
+kubectl annotate namespace ${SESSION_NAMESPACE} secretgen.carvel.dev/excluded-from-wildcard-matching-
+
+kubectl patch serviceaccount default -p '{"secrets": [{"name": "registry-credentials"},{"name": "git-https"}], "imagePullSecrets": [{"name": "registry-credentials"}]}'
+kubectl apply -f ~/samples/scan-policy.yaml
