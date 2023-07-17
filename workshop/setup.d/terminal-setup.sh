815bfde214600a98fd4172a8edf6620790a50bcf
Timo Salm <tsalm@vmware.com>
2023-07-17 17:32:09 +0200

    Add Gitea setup

--- workshop/setup.d/terminal-setup.sh
+++ workshop/setup.d/terminal-setup.sh
@@ -8,6 +8,9 @@ machine $(echo $GITEA_BASE_URL | awk -F/ '{print $3}')
        password $GITEA_PASSWORD
 EOT
 
+git config --global user.email "$GITEA_USERNAME@example.com"
+git config --global user.name "$GITEA_USERNAME"
+
 kubectl() {
     if [[ $@ == *"secret"* ]]; then
         command echo "No resources found in $SESSION_NAMESPACE namespace."
