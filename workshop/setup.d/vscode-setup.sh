2b24f8e9e1046d623ca3fbb084a46921a1da2db9
Timo Salm <tsalm@vmware.com>
2023-07-17 17:26:27 +0200

    Hide dot files in editor

--- workshop/setup.d/vscode-setup.sh
+++ workshop/setup.d/vscode-setup.sh
@@ -2,4 +2,4 @@
 set -x
 set +e
 
-jq ". + { \"java.server.launchMode\": \"Standard\", \"redhat.telemetry.enabled\": false, \"vs-kubernetes.ignore-recommendations\": true, \"tanzu-app-accelerator.tanzuApplicationPlatformGuiUrl\":  \"https://tap-gui.${TAP_INGRESS}\"}" /home/eduk8s/.local/share/code-server/User/settings.json | sponge /home/eduk8s/.local/share/code-server/User/settings.json
+jq ". + { \"java.server.launchMode\": \"Standard\", \"redhat.telemetry.enabled\": false, \"vs-kubernetes.ignore-recommendations\": true, \"tanzu-app-accelerator.tanzuApplicationPlatformGuiUrl\": \"https://tap-gui.${TAP_INGRESS}\",  \"files.exclude\": { \"**/.**\": true} }" /home/eduk8s/.local/share/code-server/User/settings.json | sponge /home/eduk8s/.local/share/code-server/User/settings.json
