a2d6ae8f8667db26297a604317a3cf5a43483f01
Timo Salm <tsalm@vmware.com>
2023-08-15 08:45:08 +0200

    Make oauth configurable in order-service

--- samples/spring-cloud-demo/order-service/src/main/java/com/example/orderservice/WebSecurityConfiguration.java
+++ samples/spring-cloud-demo/order-service/src/main/java/com/example/orderservice/WebSecurityConfiguration.java
@@ -2,17 +2,31 @@ package com.example.orderservice;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.Customizer;
 
 @Configuration
 class WebSecurityConfiguration {
 
+    @Profile("!oauth")
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf((csrf) -> csrf.disable())
                 .authorizeHttpRequests((authz) -> authz.anyRequest().permitAll())
                 .build();
+    }
+
+    @Profile("oauth")
+    @Bean
+    public SecurityFilterChain oauthFilterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests((authz) -> authz
+            .requestMatchers("/api/**").authenticated()
+            .anyRequest().permitAll()
+        )
+        .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()))
+        .build();
     }
 }
