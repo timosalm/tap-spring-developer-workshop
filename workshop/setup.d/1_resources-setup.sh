4787b75724bcda234dda862616f5d5aea0dd5a03
Timo Salm <tsalm@vmware.com>
2023-10-13 12:44:36 +0200

    Improvements

--- workshop/setup.d/1_resources-setup.sh
+++ workshop/setup.d/1_resources-setup.sh
@@ -4,7 +4,6 @@ set +e
 
 kubectl annotate namespace ${SESSION_NAMESPACE} secretgen.carvel.dev/excluded-from-wildcard-matching-
 kubectl label namespaces ${SESSION_NAMESPACE} apps.tanzu.vmware.com/tap-ns=""
-kubectl patch serviceaccount default -p '{"secrets": [{"name": "registry-credentials"}], "imagePullSecrets": [{"name": "registry-credentials"}]}'
 
 cp -a -R samples/spring-cloud-demo/. .
 rm -rf samples/spring-cloud-demo
