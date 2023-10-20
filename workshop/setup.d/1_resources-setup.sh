c2ebfc15c8af66bdc24f17408637ce5561d31db8
Timo Salm <52704922+timosalm@users.noreply.github.com>
2023-10-20 13:18:42 +0200

    Improvements

--- workshop/setup.d/1_resources-setup.sh
+++ workshop/setup.d/1_resources-setup.sh
@@ -2,7 +2,6 @@
 set -x
 set +e
 
-kubectl patch serviceaccount default -p '{"imagePullSecrets": [{"name": "educates-registry-credentials"}], "secrets": [{"name": "educates-registry-credentials"}]}'
 kubectl annotate namespace ${SESSION_NAMESPACE} secretgen.carvel.dev/excluded-from-wildcard-matching-
 kubectl label namespaces ${SESSION_NAMESPACE} apps.tanzu.vmware.com/tap-ns=""
 
