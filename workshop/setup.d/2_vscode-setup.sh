7558b06acba26db7e5a24110380524a99b208dc5
Timo Salm <52704922+timosalm@users.noreply.github.com>
2023-10-23 14:23:39 +0200

    Improvements

--- workshop/setup.d/2_vscode-setup.sh
+++ workshop/setup.d/2_vscode-setup.sh
@@ -3,18 +3,3 @@ set -x
 set +e
 
 jq ". + { \"java.server.launchMode\": \"Standard\", \"redhat.telemetry.enabled\": false, \"vs-kubernetes.ignore-recommendations\": true, \"tanzu-app-accelerator.tanzuApplicationPlatformGuiUrl\": \"https://tap-gui.${TAP_INGRESS}\", \"tanzu.sourceImage\": \"${REGISTRY_HOST}/vs-code-source-image\",  \"files.exclude\": { \"**/.**\": true}, \"editor.fontSize\": 16 }" /home/eduk8s/.local/share/code-server/User/settings.json | sponge /home/eduk8s/.local/share/code-server/User/settings.json
-
-cat <<EOL > spring-cloud-demo.code-workspace
-{
-  "folders": [
-    {
-      "name": "Order Service",
-      "path": "order-service"
-    },
-    {
-      "name": "Shipping Service",
-      "path": "shipping-service"
-    }
-  ]
-}
-EOL
