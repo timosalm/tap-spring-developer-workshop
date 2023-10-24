28158fbddcaec3fa4f40060b2107812806c9e4a8
Timo Salm <52704922+timosalm@users.noreply.github.com>
2023-10-24 15:42:41 +0200

    Improvements

--- samples/spring-cloud-demo/order-service/src/main/java/com/example/orderservice/WebSecurityConfiguration.java
+++ samples/spring-cloud-demo/order-service/src/main/java/com/example/orderservice/WebSecurityConfiguration.java
@@ -1,10 +1,17 @@
 package com.example.orderservice;
 
+import java.util.Collections;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.AbstractOAuth2Token;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.security.config.Customizer;
 
 @Configuration
@@ -28,5 +35,20 @@ class WebSecurityConfiguration {
         )
         .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()))
         .build();
+    }
+
+    @Profile("oauth")
+    @Primary
+    @Bean
+    RestTemplate oauthRestTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder.additionalInterceptors(Collections.singletonList(
+                (request, body, execution) -> {
+                  var response = execution.execute(request, body);
+                  var authentication = SecurityContextHolder.getContext().getAuthentication();
+                  var token = (AbstractOAuth2Token) authentication.getCredentials();
+                  response.getHeaders().setBearerAuth(token.getTokenValue());
+                  return response;
+                }
+        )).build();
     }
 }
